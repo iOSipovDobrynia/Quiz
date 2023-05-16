aa83f62fb96caf68d67690faec8dce97ed4188ca
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-05-16 23:37:09 +0300

    back to the previous change because bug with singleButtons

--- Quiz/ViewControllers/QuestionsViewController.swift
+++ Quiz/ViewControllers/QuestionsViewController.swift
@@ -9,33 +9,30 @@ import UIKit
 
 class QuestionsViewController: UIViewController {
     //MARK: IBOutlets
-    @IBOutlet var questionProgressView: UIProgressView!
-    @IBOutlet var questionLabel: UILabel!
-    
-    @IBOutlet var singleStackView: UIStackView!
-    @IBOutlet var singleButtons: [UIButton]!
-    
-    @IBOutlet var multipleStackView: UIStackView!
-    @IBOutlet var multipleLabels: [UILabel]!
-    @IBOutlet var multipleSwitches: [UISwitch]!
-    
-    @IBOutlet var rangedStackView: UIStackView!
-    @IBOutlet var rangedSlider: UIProgressView!
-    @IBOutlet var rangedLabels: [UILabel]!
     
     //MARK: private properties
     private let questions = Question.getQuestions()
+    private var questionIndex = 0
     
+    //MARK: UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
+//        updateUI()
     }
     
     //MARK: IBActions
-    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
-    }
-    @IBAction func multipleAnswerButtonPressed() {
-    }
-    @IBAction func rangedAnswerButtonPressed() {
-    }
+
 }
 
+////MARK: Private Methods
+//extension QuestionsViewController {
+//    private func updateUI() {
+//        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+//            stackView?.isHidden = true
+//        }
+//
+//        let currentQuestion = questions[questionIndex]
+//
+//        questionLabel.text = currentQuestion.title
+//    }
+//}
