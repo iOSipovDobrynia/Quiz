1154e70c865724ad65880b18d0734ff5f18a3216
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-05-21 20:24:01 +0300

    add calculate max repeat of animals

--- Quiz/ViewControllers/ResultViewController.swift
+++ Quiz/ViewControllers/ResultViewController.swift
@@ -10,13 +10,43 @@ import UIKit
 class ResultViewController: UIViewController {
     //MARK: properties
     var answers: [Answer] = []
+    @IBOutlet var resultAnimalLabel: UILabel!
+    @IBOutlet var resultAnimalDefinition: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setHidesBackButton(true, animated: false)
+        let resultAnimal = calculateCountAnimal(from: answers)
+        
+        resultAnimalLabel.text = "Вы – \(resultAnimal.rawValue)"
+        
+        resultAnimalDefinition.text = resultAnimal.definition
     }
     
     //MARK: IBActions
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         navigationController?.dismiss(animated: true)
+    }
+}
+
+extension ResultViewController {
+    private func calculateCountAnimal(from answers: [Answer]) -> Animal {
+        var result: Animal
+        
+        var animalCount: [Animal: Int] = [:]
+        
+        for answer in answers {
+            if let count = animalCount[answer.animal] {
+                animalCount[answer.animal] = count + 1
+            } else {
+                animalCount[answer.animal] = 1
+            }
+        }
+        
+        let sortedAnimalCount = animalCount.sorted { $0.value > $1.value }
+        
+        result = sortedAnimalCount.first?.key ?? .cat
+    
+        return result
     }
 }
